5888d09f0e2b58fef7d94bb371163c5353a117ae
Oleg Zhitenev <oleg@openres.ru>
2019-01-31 22:58:23 +0700

    1.3. JavaDoc. Code convention[#108750]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+*Class это учебный класс.
+*@author Oleg Zhieten
+*@since 2019
+*@version 1
+*/
 public class Calculate {
-	
+	/**
+	*Конструктор, вывод строки в консоль.
+	*@param args - аргументы.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
