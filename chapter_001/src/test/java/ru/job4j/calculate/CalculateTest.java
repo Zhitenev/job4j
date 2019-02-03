796d11130ac20ffc72bd84387d9cfd2650468c1b
Oleg Zhitenev <oleg@openres.ru>
2019-02-03 21:44:46 +0700

    2.5. Инспектор кода IDEA.[#108709]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -6,19 +6,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Class это учебный класс.
-*@author Oleg Zhieten
-*@since 2019
-*@version 1
-*/
+ *Class это учебный класс.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 1
+ */
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/
+	 * Test echo.
+	 */
 	@Test
 	public void whenTakeNameThenThreeEchoPlusName() {
 		String input = "Oleg Zhitenev";
-		String expect = "Echo, echo, echo : Oleg Zhitenev"; 
+		String expect = "Echo, echo, echo : Oleg Zhitenev";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
