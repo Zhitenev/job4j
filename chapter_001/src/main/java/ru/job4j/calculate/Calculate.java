db38c95198b68d09b68482aadc28c8f16bb02a19
Oleg Zhitenev <oleg@openres.ru>
2019-02-06 01:26:01 +0700

    3.1. Элементарный калькулятор.[#108741]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,27 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-*Class это учебный класс.
-*@author Oleg Zhieten
-*@since 2019
-*@version 1
-*/
+ *Class это учебный класс.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 1
+ */
 public class Calculate {
 	/**
-	*Конструктор, вывод строки в консоль.
-	*@param args - аргументы.
-	*/
+	 *Конструктор, вывод строки в консоль.
+	 *@param args - аргументы.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-	
-	 /**
-     * Method echo.
-     * @param name Your name.
-     * @return Echo plus your name.
-     */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
-	
+
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -6,21 +6,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Class это учебный класс.
-*@author Oleg Zhieten
-*@since 2019
-*@version 1
-*/
+ *Class это учебный класс.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 1
+ */
 public class CalculateTest {
-	/**
-	* Test echo.
-	*/
-	@Test
-	public void whenTakeNameThenThreeEchoPlusName() {
-		String input = "Oleg Zhitenev";
-		String expect = "Echo, echo, echo : Oleg Zhitenev"; 
-		Calculate calc = new Calculate();
-		String result = calc.echo(input);
-		assertThat(result, is(expect));
-	}
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Oleg Zhitenev";
+        String expect = "Echo, echo, echo : Oleg Zhitenev";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
