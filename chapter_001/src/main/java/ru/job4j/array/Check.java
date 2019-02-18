376d415fcc215803072f03d55cd33db9e8da2bda
Oleg Zhitenev <oleg@openres.ru>
2019-02-18 10:33:52 +0700

    6.3. Массив заполнен true или false[#108731]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+/**
+ *Проверить массив на однородность.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 1
+ */
+public class Check {
+    /**
+     * Проверка однородности массива.
+     * @param data входной масив.
+     * @return результат проверки на однородность.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        for (int i = 0; i < data.length - 1; i++) {
+            result = data[i] == data[i + 1];
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ *Test класса Check.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 1
+ */
+public class CheckTest {
+    /**
+     * Тест когда массив однородный.
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Тест когда массив не однородный.
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
