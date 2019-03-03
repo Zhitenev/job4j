aa26c876ce6906ec7902a409442b76fd439757bd
Oleg Zhitenev <oleg@openres.ru>
2019-03-03 13:57:51 +0700

    Объеденить 3 отсортированных массива в 1.

--- chapter_001/src/main/java/ru/job4j/array/SortArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/SortArrays.java
@@ -20,60 +20,23 @@ public class SortArrays {
         int a = 0;
         int b = 0;
         int c = 0;
-        int max = maxFromThree(firsArray[fLen - 1], secondArray[sLen - 1], thirdArray[tLen - 1]);
+        int max = Integer.max(Integer.max(firsArray[fLen - 1], secondArray[sLen - 1]), thirdArray[tLen - 1]);
         for (int i = 0; i < result.length; i++) {
-            int min = minFromThree(firsArray[a], secondArray[b], thirdArray[c]);
+            int min = Integer.min(Integer.min(firsArray[a], secondArray[b]), thirdArray[c]);
             if (min == firsArray[a]) {
                 result[i] = firsArray[a];
-                if (a < firsArray.length - 1) {
-                    a++;
-                } else {
-                    firsArray[a] = max;
-                }
-
-            } else if (min == secondArray[b]) {
-                result[i] = secondArray[b];
-                if (b < secondArray.length - 1) {
-                    b++;
-                } else {
-                    secondArray[b] = max;
-                }
-            } else if (min == thirdArray[c]) {
-                result[i] = thirdArray[c];
-                if (c < thirdArray.length - 1) {
-                    c++;
-                } else {
-                    thirdArray[c] = max;
-                }
-
+                firsArray[a] = a == fLen - 1 ? max : firsArray[a];
+                a = a < (fLen - 1) ? ++a : a;
+                } else if (min == secondArray[b]) {
+                    result[i] = secondArray[b];
+                    secondArray[b] = b == sLen - 1 ? max : secondArray[b];
+                    b = b < (sLen - 1) ? ++b : b;
+                    } else if (min == thirdArray[c]) {
+                        result[i] = thirdArray[c];
+                        thirdArray[c] = c == tLen - 1 ? max : thirdArray[c];
+                        c = c < tLen - 1 ? ++c : c;
             }
         }
-        return result;
-    }
-
-    /**
-     * Найдем минимум из 3х чисел.
-     * @param first первый аргумент.
-     * @param second второй агрумент.
-     * @param third третий аргумент.
-     * @return минимальное число.
-     */
-    private int minFromThree(int first, int second, int third) {
-            int one = first < second ? first : second;
-            int two = second < third ? second : third;
-        return one < two ? one : two;
-    }
-
-    /**
-     * Найдем максимум из 3х чисел.
-     * @param first первый аргумент.
-     * @param second второй агрумент.
-     * @param third третий аргумент.
-     * @return максимальное число.
-     */
-    private  int maxFromThree(int first, int second, int third) {
-        int a = first > second ? first : second;
-        int b = second > third ? second : third;
-        return a > b ? a : b;
+    return result;
     }
 }
