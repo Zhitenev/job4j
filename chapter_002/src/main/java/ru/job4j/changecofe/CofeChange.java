fcbe4c0722cbc7fcf0133e754d3e3fbd23d78d8c
Oleg Zhitenev <oleg@openres.ru>
2019-04-02 19:05:14 +0700

    Кофемашина. [#34741]

--- chapter_002/src/main/java/ru/job4j/changecofe/CofeChange.java
+++ chapter_002/src/main/java/ru/job4j/changecofe/CofeChange.java
@@ -1,13 +1,11 @@
 package ru.job4j.changecofe;
-
+import java.util.Arrays;
  /**
  *Выдача сдачи кофемашиной.
  *@author Oleg Zhieten
  *@since 2019
  *@version 2
  */
-
-import java.util.Arrays;
 
 public class CofeChange {
     /**
@@ -16,29 +14,18 @@ public class CofeChange {
      * @param price стоимость товара.
      * @return массив с оптимальным колличеством и номиналом монет.
      */
-    int[] changes(int value, int price) {
-        int change = value - price;
-        int[] result = new int[change];
-        int j = 0;
-        for (int i = 0; i < value; i++) {
-            if ((change - 10) >= 0 || (change - 10) == 1) {
-                result[i] = 10;
-                change = change - 10;
-                j++;
-            } else if ((change - 5) >= 0) {
-                result[i] = 5;
-                change = change - 5;
-                j++;
-            } else if ((change - 2) >= 0 & change != 1) {
-                result[i] = 2;
-                change = change - 2;
-                j++;
-            } else if ((change - 1) == 0) {
-                result[i] = 1;
-                j++;
-                break;
-            }
-        }
-        return Arrays.copyOf(result, j);
-    }
+
+     int[] changes (int value, int price) {
+         int[] COINS = new int[] {10, 5, 2, 1};
+         int change = value - price;
+         int[] result = new int[change];
+         int i = 0;
+         for (int coin : COINS) {
+             while (change >= coin) {
+                 result[i++] = coin;
+                 change = change - coin;
+             }
+         }
+         return Arrays.copyOf(result, i);
+     }
 }
