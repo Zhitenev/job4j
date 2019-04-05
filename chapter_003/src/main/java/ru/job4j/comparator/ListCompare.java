12219672dddab264b72a0015325b57c48a76cfbc
Oleg Zhitenev <oleg@openres.ru>
2019-04-05 14:50:02 +0700

    3. Компаратор для строк.[#108764]

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -1,8 +1,7 @@
 package ru.job4j.comparator;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
+
 /**
  *Реализация класса Компаратор для строк.
  *@since 2019
@@ -11,23 +10,15 @@ import java.util.List;
 public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        List<Character> first = new ArrayList<>();
-        List<Character> second = new ArrayList<>();
-        for (char lf : left.toCharArray()) {
-            first.add(lf);
-        }
-        for (char rg : right.toCharArray()) {
-            second.add(rg);
-        }
         int result = 0;
-        int size = first.size() < second.size() ? first.size() : second.size();
+        int size = left.length() < right.length() ? left.length() : right.length();
             for (int i = 0; i < size; i++) {
-                result = Character.compare(first.get(i), second.get(i));
+                result = Character.compare(left.charAt(i), right.charAt(i));
                 if (result != 0) {
                     break;
                 }
             }
-            if (result == 0 && first.size() != second.size()) {
+            if (result == 0 && left.length() != right.length()) {
                 result = -1;
             }
         return result;
