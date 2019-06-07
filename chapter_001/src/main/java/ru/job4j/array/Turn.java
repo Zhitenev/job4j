f8df96143aa922f31ecf3588776a7c980d25b87b
Oleg Zhitenev <oleg@openres.ru>
2019-06-07 19:21:38 +0700

    0. Stream API & Update Java && clean errors

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -15,7 +15,7 @@ public class Turn {
     public int[] back(int[] array) {
         for (int i = 0; i < array.length / 2; i++) {
             int tmp = array[array.length - i - 1];
-            array[array.length - i - 1] = array [i];
+            array[array.length - i - 1] = array[i];
             array[i] = tmp;
         }
         return array;
--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,6 +2,7 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *Реализация класса телефонный справочник.
@@ -22,13 +23,17 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public List<Person> find(String key) {
-        List<Person> result = new ArrayList<>();
+/**        List<Person> result = new ArrayList<>();
             for (Person pers : this.persons) {
                 String str = pers.getAddress() + pers.getName() + pers.getPhone() + pers.getSurname();
                 if (str.contains(key)) {
                     result.add(pers);
                 }
             }
+ **/
+        List<Person> result = this.persons.stream().filter(
+                pers -> (pers.getAddress() + pers.getName() + pers.getPhone() + pers.getSurname()).contains(key)
+        ).collect(Collectors.toList());
         return result;
     }
 }
