59f32e686ae520b60b3da310806788ec13093fa9
Oleg Zhitenev <oleg@openres.ru>
2019-07-03 10:14:16 +0700

    1. Collection API Улучшения[#108685]

--- chapter_003/src/main/java/ru/job4j/sort/User.java
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -27,11 +27,15 @@ public class User implements Comparable<User> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
-        return age.equals(user.age) &&
-                name.equals(user.name);
+        return age.equals(user.age)
+                && name.equals(user.name);
     }
 
     @Override
