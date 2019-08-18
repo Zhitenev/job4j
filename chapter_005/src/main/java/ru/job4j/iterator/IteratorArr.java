42ea4447232ca9fc9b65cc8d358562a55790ffa8
Oleg Zhitenev <oleg@openres.ru>
2019-08-18 15:58:56 +0700

    5.1.1. Итератор для двухмерного массива int[][]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/IteratorArr.java
@@ -0,0 +1,52 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Рализация итерации двумерного массива.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 2
+ */
+public class IteratorArr implements Iterator {
+    private int[][] values;
+    private int index = 0;
+    private int nes = 0;
+
+    public IteratorArr(int[][] values) {
+        this.values = values;
+    }
+
+    /**
+     * Проверка возможности следующей итерации.
+     * @return вероятность сдвига.
+     */
+    @Override
+    public boolean hasNext() {
+        boolean result;
+        if (nes == values.length - 1 & index == values[values.length - 1].length) {
+            result = false;
+        } else {
+            result = values[nes].length >= index;
+        }
+        return result;
+    }
+
+    /**
+     * Получаем элемент и переводим коретку.
+     * @return следующий элемент.
+     */
+    @Override
+    public Object next() {
+        try {
+            if (!(values[nes].length > index)) {
+                this.nes++;
+                this.index = 0;
+            }
+            return values[nes][index++];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+}
