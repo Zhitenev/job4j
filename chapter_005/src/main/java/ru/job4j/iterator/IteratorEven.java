1be1312633c059ef38d3ce49e81c60b99b578794
Oleg Zhitenev <oleg@openres.ru>
2019-08-19 12:25:20 +0700

    5.1.2. Создать итератор четные числа [#150]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/IteratorEven.java
@@ -0,0 +1,62 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * Рализация итерации массива по четным числам.
+ *@author Oleg Zhieten
+ *@since 2019
+ *@version 2
+ */
+public class IteratorEven implements Iterator {
+
+    private int[] values;
+    private int index = 0;
+
+    public IteratorEven(int[] values) {
+        this.values = values;
+    }
+
+    /**
+     * Проверка возможности следующей итерации.
+     * @return вероятность сдвига.
+     */
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        for (int i = index; i < values.length; i++) {
+            if (values[i] % 2 == 0) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Получаем элемент и переводим коретку.
+     * @return следующий элемент.
+     */
+    @Override
+    public Object next() {
+        try {
+            for (int i = index; i < values.length + 1; i++) {
+                if (values[i] % 2 == 0) {
+                    this.index = i;
+                    break;
+                }
+            }
+            return values[index++];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * Метод remove без реализации.
+     */
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
