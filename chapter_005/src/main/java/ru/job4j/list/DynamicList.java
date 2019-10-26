b293c9f03ef94337db5aaa664431dd7793729726
oleg@openres.ru <asdm371K>
2019-10-26 08:28:58 +0700

    5.3.1. Создать динамический список на базе массива. [#158]

--- chapter_005/src/main/java/ru/job4j/list/DynamicList.java
+++ chapter_005/src/main/java/ru/job4j/list/DynamicList.java
@@ -1,4 +1,78 @@
 package ru.job4j.list;
 
-public class DynamicList {
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicList<T> implements Iterable<Object> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static int modCount = 0;
+    private Object[] objects = new Object[DEFAULT_CAPACITY];
+    private int index = 0;
+
+    void add(T value) {
+            this.getMoreSize();
+            this.objects[index++] = value;
+    }
+
+    T get(int index) {
+        return (T) this.objects[index];
+    }
+
+    Integer size() {
+        return this.index;
+    }
+
+    private void getMoreSize() {
+        if (objects.length >= index) {
+            this.objects = Arrays.copyOf(this.objects, this.index + DEFAULT_CAPACITY);
+            modCount++;
+        }
+    }
+
+    /**
+     * Реализация Iterable.
+     * @return  возвращает итератор для массива с неопределенным типом данных.
+     */
+    @Override
+    public Iterator<Object> iterator() {
+        return new DynamicList.DynamicListIterator();
+    }
+
+    /**
+     * Реализация итератора.
+     */
+    class DynamicListIterator implements Iterator<T> {
+        /**
+         * Переопределение метода hasNext.
+         * @return возможно смещение или нет.
+         */
+        private int expectedModCount = modCount;
+        @Override
+        public boolean hasNext() {
+            boolean result;
+            if (objects.length == 0) {
+                result = false;
+            } else {
+                result = objects.length > index;
+            }
+            return result;
+        }
+
+        /**
+         * Переопредиление метода next.
+         * @return смещает коректку или выдает ошибку что не существует ячейки.
+         */
+        @Override
+        public T next() {
+            if (hasNext()) {
+                if (expectedModCount != modCount) {
+                  throw new ConcurrentModificationException();
+                }
+                return (T) objects[index++];
+            }
+            throw new NoSuchElementException();
+        }
+    }
 }
--- chapter_005/src/test/java/ru/job4j/list/DynamicListTest.java
+++ chapter_005/src/test/java/ru/job4j/list/DynamicListTest.java
@@ -1,4 +1,31 @@
 package ru.job4j.list;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class DynamicListTest {
+    private DynamicList<Integer> dn = new DynamicList<>();
+    @Before
+    public void beforeTest() {
+        for (int j = 0; j < 10; j++) {
+            dn.add(j);
+        }
+    }
+
+    @Test
+    public void whenAddMoreThenTenElements() {
+        dn.add(10);
+        assertThat(dn.size(), is(11));
+    }
+
+    @Test
+    public void whenAddMoreThenTwentyElements() {
+        for (int j = 10; j < 20; j++) {
+            dn.add(j);
+        }
+        dn.add(20);
+        assertThat(dn.size(), is(21));
+    }
 }
