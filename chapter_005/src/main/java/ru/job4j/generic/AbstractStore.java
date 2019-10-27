a6a9c69db6108a5273d9f26b3eeec27c5f7afb8b
Oleg Zhitenev <oleg@openres.ru>
2019-10-27 14:09:59 +0700

    5.2.2. Реализовать Store<T extends Base> [#157][#191629]

--- chapter_005/src/main/java/ru/job4j/generic/AbstractStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/AbstractStore.java
@@ -1,26 +1,36 @@
 package ru.job4j.generic;
 
-public abstract class AbstractStore implements Store {
-    private SimpleArray<Base> baseStore = new SimpleArray<>(10);
+public abstract class AbstractStore<E extends Base> implements Store<E> {
+    private SimpleArray<E> baseStore = new SimpleArray<>(100);
     @Override
-    public void add(Base value) {
+    public void add(E value) {
         baseStore.add(value);
     }
 
     @Override
-    public boolean replace(String id, Base value) {
-        baseStore.set(Integer.parseInt(id), value);
+    public boolean replace(String id, E value) {
+        baseStore.set(getId(id), value);
         return true;
     }
 
     @Override
     public boolean delete(String id) {
-        baseStore.remove(Integer.parseInt(id));
+        baseStore.remove(getId(id));
         return true;
     }
 
     @Override
-    public Base findById(String id) {
-        return baseStore.get(Integer.parseInt(id));
+    public E findById(String id) {
+        return (E) baseStore.get(getId(id));
+    }
+
+    private int getId(String id) {
+        int pos = 0;
+        for (E c : baseStore) {
+            if (c.getId().equals(id)) {
+                pos++;
+            }
+        }
+        return pos;
     }
 }
