ce61dd2e4418c4d6e31290465c03f44df5174d97
Oleg Zhitenev <oleg@openres.ru>
2019-11-06 21:15:22 +0700

    4. Переопределить только equals [#1004]

--- chapter_005/src/main/java/ru/job4j/map/MapUser.java
+++ chapter_005/src/main/java/ru/job4j/map/MapUser.java
@@ -6,7 +6,7 @@ public class MapUser extends User {
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public boolean equals(Object o) {
+        return super.equals(o);
     }
 }
--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -13,7 +13,10 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
     }
 }
