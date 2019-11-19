5043cefebab873546df00a463c926ed19c2bd9c0
Oleg Zhitenev <oleg@openres.ru>
2019-11-19 14:08:39 +0700

    1. Создать элементарную структуру дерева [#1711]

---
+++ chapter_005/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,84 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    private Node<E> root;
+
+    private int modCount = 0;
+
+    public Tree(E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        if (findBy(child).isEmpty()) {
+            Optional<Node<E>> optionalNode = findBy(parent);
+            if (optionalNode.isPresent()) {
+                optionalNode.get().add(new Node<>(child));
+                result = true;
+                modCount++;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.eqValue(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            for (Node<E> child : element.leaves()) {
+                data.offer(child);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Tree.TreeIterator();
+    }
+
+    private class TreeIterator implements Iterator<E> {
+
+        private int expectedModCount = modCount;
+
+        private Queue<Node<E>> data = new LinkedList<>();
+
+        private TreeIterator() {
+            data.offer(root);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !data.isEmpty();
+        }
+
+        @Override
+        public E next() {
+            Node<E> element;
+            if (hasNext()) {
+                if (expectedModCount < modCount) {
+                    element = data.poll();
+                    for (Node<E> child : element.leaves()) {
+                        data.offer(child);
+                    }
+                    return element.getValue();
+                }
+                throw new ConcurrentModificationException();
+            }
+            throw new NoSuchElementException();
+        }
+
+    }
+}
