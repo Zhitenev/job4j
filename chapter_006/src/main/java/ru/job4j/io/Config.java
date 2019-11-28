7520429f5c0b0fcc2ebb9fa8c86a0b065ae8d35d
Oleg Zhitenev <oleg@openres.ru>
2019-11-28 23:34:50 +0700

    1. Читаем файл конфигурации [#858]

---
+++ chapter_006/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] strArr = line.split("\\=");
+                if (strArr.length == 2) {
+                    values.put(strArr[0], strArr[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String value(String key) {
+        return this.values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("/Users/ozh/Documents/job4j/chapter_006/data/app.properties"));
+    }
+}
