5acc493308224a421aa492b5295f7163c561d83c
Oleg Zhitenev <oleg@openres.ru>
2019-12-23 12:23:40 +0700

    3. Сканирование файловой системы. [#106929]

---
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+    private static List<File> result = new ArrayList<>();
+    List<File> files(String parent, List<String> exts) {
+
+        File file = new File(parent);
+        File[] list = file.listFiles();
+
+        if (list != null && !file.isDirectory()) {
+
+            throw new IllegalArgumentException(
+                    "Cannot list content of "
+                            + file.getPath()
+                            + ": Not a directory");
+        }
+
+        for (File f : list) {
+            if (f.isDirectory()) {
+                files(f.getAbsolutePath(), exts);
+            } else {
+                for (String ext : exts) {
+                    if (ext == null || f.getName().contains(ext)) {
+                        result.add(f);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SearchTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenTestInListTxt() throws IOException {
+        folder.newFile("myfile.txt");
+        folder.newFolder("subfolder");
+        folder.newFile("subfolder/myfile2.txt");
+        folder.newFolder("subfolder", "subSubfolder");
+        folder.newFile("subfolder/subSubfolder/myfile.java");
+        Search search = new Search();
+        List<String> ext = new ArrayList<>();
+        ext.add(".txt");
+        ext.add(".java");
+        List<File> files = new ArrayList<>(search.files(folder.getRoot().getPath(), ext));
+        assertThat(
+                files.get(1).getName(),
+                is("myfile2.txt")
+        );
+    }
+
+    @Test
+    public void whenTestInListJava() throws IOException {
+        folder.newFile("myfile.txt");
+        folder.newFolder("subfolder");
+        folder.newFile("subfolder/myfile2.txt");
+        folder.newFolder("subfolder", "subSubfolder");
+        folder.newFile("subfolder/subSubfolder/myfile.java");
+        Search search = new Search();
+        List<String> ext = new ArrayList<>();
+        ext.add(".txt");
+        ext.add(".java");
+        List<File> files = new ArrayList<>(search.files(folder.getRoot().getPath(), ext));
+        assertThat(
+                files.get(0).getName(),
+                is("myfile.java")
+        );
+    }
+}
